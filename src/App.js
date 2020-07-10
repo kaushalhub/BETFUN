a584baada23ac9a16783a55fc85fe782e68f8bc4
kaushalhub <jainkaushal123456@gmail.com>
2020-07-10 18:17:03 +0530

    fg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
@@ -21,37 +21,44 @@ import Userpl from './components/Userpl';
 import ProfitLoss from './components/ProfitLoss';
 import Livegame from './components/Livegame';
 import Fancystack from './components/Fancystack';
-
-const App = () => {
+const App = (props) => {
   return (
-     <Router>
-           <Fragment>
-        <Navbar />
-        <Sidebar />
-        <Route exact path='/' component={Dashboard} />
-       <section>
-       <Switch>
-          <Route exact path='/master' component={Master} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/user" component={User} />
-          <Route exact path="/closeuser" component={CloseUser} />
-          <Route exact path="/blockmarket" component={BlockMarket} />
-          <Route exact path="/mymarket" component={MyMarket} />
-          <Route exact path="/bethistory" component={Bethistory} />
-          <Route exact path="/acinfo" component={Acinfo} />
-          <Route exact path="/clientpl" component={Clientpl} />
-          <Route exact path="/cacstatement" component={Cacstatement} />
-          <Route exact path="/marketpl" component={Marketpl} />
-          <Route exact path="/sportspl" component={Sportspl} />
-          <Route exact path="/userpl" component={Userpl} />
-          <Route exact path="/profitloss" component={ProfitLoss} />
-          <Route exact path="/livegame" component={Livegame} />
-          <Route exact path="/fancystack" component={Fancystack} />
-           </Switch> 
-       </section>
+   //   <Router>
+   //      {/* {
+   //    props.location.path!=='/login' ? <Navbar/>:null
+   //   } */}
+   //         <Fragment>
+   //      <Navbar />
+   //      <Sidebar />
+   //      <Route exact path='/' component={Dashboard} />
+   //     <section>
+   //     <Switch>
+   //        <Route exact path='/master' component={Master} />
+   //        <Route exact path="/login" component={Login} />
+   //        <Route exact path="/user" component={User} />
+   //        <Route exact path="/closeuser" component={CloseUser} />
+   //        <Route exact path="/blockmarket" component={BlockMarket} />
+   //        <Route exact path="/mymarket" component={MyMarket} />
+   //        <Route exact path="/bethistory" component={Bethistory} />
+   //        <Route exact path="/acinfo" component={Acinfo} />
+   //        <Route exact path="/clientpl" component={Clientpl} />
+   //        <Route exact path="/cacstatement" component={Cacstatement} />
+   //        <Route exact path="/marketpl" component={Marketpl} />
+   //        <Route exact path="/sportspl" component={Sportspl} />
+   //        <Route exact path="/userpl" component={Userpl} />
+   //        <Route exact path="/profitloss" component={ProfitLoss} />
+   //        <Route exact path="/livegame" component={Livegame} />
+   //        <Route exact path="/fancystack" component={Fancystack} />
+   //         </Switch> 
+   //     </section>
          
-         </Fragment>
-      </Router>   
+   //       </Fragment>
+   //    </Router>   
+
+
+   <div className="App">
+      <Login />
+   </div>
 
   );
 }   
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-
+import '../Login.css'
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+
+        <div className="body">
+        <form class="form-login">
+                        <h1 class="h3 mb-3 font-weight-normal">Welcome To Login</h1>
+                        <label for="username" class="sr-only">Username</label>
+                        <input type="text" class="form-control mb-2" name="username" placeholder="Enter Username" /><br />
+
+
+                        <label for="password" class="sr-only">Password</label>
+                        <input type="password" class="form-control mb-2" name="password" placeholder="Enter Password" /><br />
+
+                        <button type="submit" class="btn btn-primary btn-block" name="submit">Login</button>
+                    </form>
+        </div>            
     )
 }
 
